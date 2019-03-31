19cce2aa9e761ee06e5f3b0d7135ac27243de5ba
Daniel Dopierała <dopierala.daniel@gmail.com>
2019-03-31 14:50:53 +0200

    Initial commit from Spring Initializr

---
+++ src/main/kotlin/pl/dopierala/bio/sbh/SbhApplication.kt
@@ -0,0 +1,11 @@
+package pl.dopierala.bio.sbh
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SbhApplication
+
+fun main(args: Array<String>) {
+	runApplication<SbhApplication>(*args)
+}
