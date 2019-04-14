8f3498d78eaf4f1dffded8b5936229a5b8f972b9
Daniel Dopierała <dopierala.daniel@gmail.com>
2019-04-14 15:52:10 +0200

    Refactor DNA utilities to be written in more functional style

--- src/main/kotlin/pl/dopierala/bio/sbh/model/Dna.kt
+++ src/main/kotlin/pl/dopierala/bio/sbh/model/Dna.kt
@@ -1,14 +1,3 @@
 package pl.dopierala.bio.sbh.model
 
-object Dna {
-
-    const val adenine = "A"
-    const val cytosine = "C"
-    const val guanine = "G"
-    const val thymine = "T"
-    val nucleotides = setOf(adenine, cytosine, guanine, thymine)
-
-    fun generate(length: Int): String {
-        return List(length) { nucleotides.random() }.joinToString("")
-    }
-}
+fun generateDna(length: Int): String = List(length) { randomNucleotide() }.joinToString("")
--- src/test/kotlin/pl/dopierala/bio/sbh/model/DnaTest.kt
+++ src/test/kotlin/pl/dopierala/bio/sbh/model/DnaTest.kt
@@ -1,6 +1,7 @@
 package pl.dopierala.bio.sbh.model
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -9,15 +10,14 @@ internal class DnaTest {
 
     @ParameterizedTest
     @ValueSource(ints = [0, 1, 2, 5, 10, 100, 700, 1000])
-    fun `WHEN generating DNA THEN generates a random String`(length: Int) {
-        val dna = Dna.generate(length)
+    fun `Generates valid DNA sequences`(length: Int) {
+        val dna = generateDna(length)
 
         assertAll(
-                { assertEquals(length, dna.length, "DNA has invalid length!") },
+                { assertEquals(length, dna.length) },
                 {
                     val distinctNucleotides = dna.chunked(1).toSet()
-                    val unknownNucleotides = distinctNucleotides.subtract(Dna.nucleotides)
-                    assertEquals(emptySet<String>(), unknownNucleotides, "DNA contains unknown nucleotides!")
+                    assertTrue(nucleotides.containsAll(distinctNucleotides))
                 }
         )
     }
