f1a1db4f261398d7a3f3e9a6f6739c2fc545e1c2
Daniel Dopierała <dopierala.daniel@gmail.com>
2019-09-26 13:47:12 +0200

    Actually solve the SBH problem using the "solve" command

--- src/main/kotlin/pl/dopierala/bio/sbh/shell/SbhCommands.kt
+++ src/main/kotlin/pl/dopierala/bio/sbh/shell/SbhCommands.kt
@@ -1,15 +1,22 @@
 package pl.dopierala.bio.sbh.shell
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.shell.standard.ShellCommandGroup
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
 import org.springframework.shell.standard.ShellOption
+import pl.dopierala.bio.sbh.solution.Solver
+import pl.dopierala.bio.sbh.solution.model.Instance
+import pl.dopierala.bio.sbh.verification.scoreAgainst
 import javax.validation.constraints.Max
 import javax.validation.constraints.Min
 
 @ShellComponent
 @ShellCommandGroup("SBH commands")
-class SbhCommands {
+class SbhCommands(private val solver: Solver) {
+
+    private val logger: Logger = LoggerFactory.getLogger(SbhCommands::class.java)
 
     @ShellMethod(value = "Solves the SBH problem.")
     fun solve(@ShellOption(value = ["-d", "--dna-length"], defaultValue = "100", help = "Length of the DNA sequence.")
@@ -21,6 +28,15 @@ class SbhCommands {
               @Min(8)
               @Max(12)
               oligonucleotideLength: Int) {
-        println("DNA length: $dnaLength, oligonucleotide length: $oligonucleotideLength.")
+        val instance = Instance(dnaLength, oligonucleotideLength)
+        logger.info("DNA length: {}", dnaLength)
+        logger.info("Oligonucleotide length: {}", oligonucleotideLength)
+        logger.info("Original sequence: {}", instance.dna)
+
+        val sequence = solver.solve(instance)
+        logger.info("Computed sequence: {}", sequence)
+
+        val score = sequence.scoreAgainst(instance.dna)
+        logger.info("Achieved score: {}%", score)
     }
 }
